d8a30ad49dce145896a22db81a013fe8d0e40aab
Devdeep8 <devdeeppatidar8@gmail.com>
2026-01-14 15:23:43 +0530

    feat: connected rabbitmq

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 6001;
 import cors from 'cors';
 import { userRouter } from './routes/user.routes/route.js';
+import { connectDB } from './config/db.js';
+import { connectRabbitMQ } from './config/rabbitmq.js';
 app.use(express.json());
 app.use(cors());
-
-// await connectDB()
+await connectDB()
+await connectRabbitMQ()
 app.use('/api/user', userRouter);
 
 app.listen(PORT, () => {
